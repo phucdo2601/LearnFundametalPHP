5d23094799f01d16599117300ccaf38c6df1449e
Phuc Do <ngocphucdo2601@gmail.com>
2023-02-23 09:07:23 +0700

    Config basic php connect with mysql workbench b01

--- feedback/configs/database.php
+++ feedback/configs/database.php
@@ -1,6 +1,15 @@
 <?php
-$servername = "127.0.0.1:3307";
-$username = "phucdo";
+
+//connect to mysql on XAMMP
+// $servername = "127.0.0.1:3307";
+// $username = "phucdo";
+// $password = "12345678";
+// $databaseName = "learn_php_feedback_b01";
+
+
+//connect to mysql workbench
+$servername = "127.0.0.1:3306";
+$username = "root";
 $password = "12345678";
 $databaseName = "learn_php_feedback_b01";
 
