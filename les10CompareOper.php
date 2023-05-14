5e147ef868479cfebc71b72a132f0c8587d8640d
Phuc Do <ngocphucdo2601@gmail.com>
2023-05-14 20:55:04 +0700

    10-Comparasion Operator b01

---
+++ les10CompareOper.php
@@ -0,0 +1,35 @@
+<?php
+
+// $a = 3;
+
+// $a %= 2;
+
+// echo $a;
+
+
+// $a = "Hello";
+
+// $a .= " World";
+
+// echo $a;
+
+$a = '3';
+
+/**
+ * <, > greater than
+ * == value equality
+ * === value + type equality
+ * 
+ */
+
+// if ($a === 3) {
+//     echo "This is bigger than 3";
+// } else {
+//     echo "This is not bigger than 3";
+// }
+
+if ($a === 3) {
+    echo "This is 3";
+} else {
+    echo "This is not 3";
+}
