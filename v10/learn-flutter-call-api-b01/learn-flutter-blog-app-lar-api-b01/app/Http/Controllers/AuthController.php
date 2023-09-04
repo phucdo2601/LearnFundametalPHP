63a60724614ddfb50180d87999aaaee142b8b6ee
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2023-09-04 14:09:19 +0700

    [laravel-api-blog-app-flutter-b01] 01-Intial and basic set up project b01

---
+++ v10/learn-flutter-call-api-b01/learn-flutter-blog-app-lar-api-b01/app/Http/Controllers/AuthController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    // Register user function
+    public function register(Request $request) {
+
+        // Validate fields
+        $attrs = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6|confirmed',
+        ]);
+
+        //create user
+        $user = User::create([
+            'name' => $attrs['name'],
+            'email' => $attrs['email'],
+            'password' => $attrs['password'],
+        ]);
+
+        //return user
+        return response([
+            'user' => $user,
+            'token' => $user->createToken('secret')->plainTextToken,
+        ]);
+    }
+
+    public function login(Request $request){
+        // Validate fields
+        $attrs = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:6|confirmed',
+        ]);
+
+        // login failed
+        if (!Auth::attempt($attrs)) {
+            return response([
+                'message' => 'Invalid credentials.'
+            ], 403);
+        }
+
+        //return user
+        return response([
+            'user' => auth()->user(),
+            'token' => auth()->user()->createToken('secret')->plainTextToken,
+        ],200);
+    }
+
+    //logout func
+    public function logout() {
+        auth()->user()->tokens()->delete();
+        return response([
+            'message' => 'Logout Success'
+        ], 200);
+    }
+
+    //get user details
+    public function user() {
+        return response([
+            'user' => auth()->user()
+        ]);
+    }
+}
