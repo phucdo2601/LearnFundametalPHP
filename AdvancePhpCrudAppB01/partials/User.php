80cc3bbe2d457d5bc6a9a90458c48ebb2dcecf94
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2023-10-02 11:20:24 +0700

    [ad-php-crud-b01] 03.Basic update add new record func b01

--- AdvancePhpCrudAppB01/partials/User.php
+++ AdvancePhpCrudAppB01/partials/User.php
@@ -2,6 +2,59 @@
     require_once './connect.php';
 
     class User extends Database {
+        protected $tableName = "user";
+
+        //functions to add users
+        public function add($data) {
+            if (!empty($data)) {
+                # code...
+                $fileds = $placeholder = [];
+                foreach ($data as $field => $value) {
+                    $fileds[] = $field;
+                    $placeholder[] = ":{field}";
+                }
+            }
+
+            // $sql = "INSERT INTO {$this->tableName} (name, email, mobile) values (:name, :email, :mobile);";
+            $sql = "INSERT INTO {$this->tableName} (" .implode(",", $fileds) .") values (".implode(",", $placeholder) .")";
+
+            $stmt =$this->conn->prepare($sql);
+
+            try {
+                $this->conn->beginTransaction();
+                
+                $stmt->execute($data);
+
+                $lastInsertedId = $this->conn->lastInsertId();
+
+                $this->conn->commit();
+
+                return $lastInsertedId;
+
+            } catch (PDOException $e) {
+                echo "Cannot executing add new user function: ".$e->getMessage();
+                $this->conn->rollBack();
+            }
+        }
+
+
+        //function to get rows
+
+
+        // function to get single row
+
+
+        // function to count number of rows
+
+
+        //function to upload photo
+
+
+        //function to update user
         
+
+        //function to delete user
+
+        // function for search
     }
 ?>
