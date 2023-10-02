521de3bb1ce44f5e11a8ccc4e564a4aba2049d5a
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2023-10-02 11:54:01 +0700

    [ad-php-crud-b01] 05.Update basic upload image b01

--- AdvancePhpCrudAppB01/partials/User.php
+++ AdvancePhpCrudAppB01/partials/User.php
@@ -81,6 +81,29 @@
 
 
         //function to upload photo
+        public function uploadPhoto($file) {
+            if (!empty($file)) {
+                $fileTempPath = $file['tmp_name'];
+                $fileName = $file['name'];
+                $fileType = $file['type'];
+
+                $fileNameCmps = explode('.', $fileName);
+                $fileExtension = strtolower(end($fileNameCmps));
+                $newFileName = md5(time().$fileName) . '.' .$fileExtension;
+                $allowedExtn = ["png", "jpg", "jpeg"];
+
+
+                if (in_array($fileExtension, $allowedExtn)) {
+                    $uploadFileDir = getcwd() . '/uploads/';
+                    $destFilePath = $uploadFileDir . $newFileName;
+
+                    if (move_uploaded_file($fileTempPath, $destFilePath)) {
+                        return $newFileName;
+                    }
+                }
+
+            }
+        }
 
 
         //function to update user
