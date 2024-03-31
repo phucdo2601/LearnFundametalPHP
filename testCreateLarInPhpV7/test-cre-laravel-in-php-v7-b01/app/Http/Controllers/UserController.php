096ce9bf5fbd7d73151e9e7f3e0ff3a94cbc5175
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2024-03-31 16:30:38 +0700

    [learn-laravel-v7] 07. First learn basic Accessor and Mutators b01

--- testCreateLarInPhpV7/test-cre-laravel-in-php-v7-b01/app/Http/Controllers/UserController.php
+++ testCreateLarInPhpV7/test-cre-laravel-in-php-v7-b01/app/Http/Controllers/UserController.php
@@ -61,13 +61,15 @@ class UserController extends Controller
         // return view('home');
 
         //  Mass Assignment Security
-        $data_ins = [
-            'name' => 'testName08',
-            'email' => 'testEmail08@gmail.com',
-            'password' => "12345678",
-        ];
+        // User::where('email', 'testEmail08@gmail.com')->delete();
 
-        User::create($data_ins);
+        // $data_ins = [
+        //     'name' => 'testName08',
+        //     'email' => 'testEmail08@gmail.com',
+        //     'password' => "12345678",
+        // ];
+
+        // User::create($data_ins);
 
         $listUsers = User::all();
 
