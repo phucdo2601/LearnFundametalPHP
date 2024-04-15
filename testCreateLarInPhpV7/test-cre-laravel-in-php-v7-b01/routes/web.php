5d132f4c4a16f4af4e6a8e3349fde2b500219000
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2024-04-15 21:49:44 +0700

    [learn-laravel-v7] 10.Learn basic laravel configuration b01

--- testCreateLarInPhpV7/test-cre-laravel-in-php-v7-b01/routes/web.php
+++ testCreateLarInPhpV7/test-cre-laravel-in-php-v7-b01/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\View;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return View::make('welcome');
 });
 
 Route::get('/user', 'UserController@index');
