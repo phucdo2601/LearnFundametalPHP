8fa70cab6bd88afaf7b3d20a62f1cc9dc535b01a
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2024-10-01 15:57:48 +0700

    [learn-php-lar-v11] 03. Basic learn routing in php b01

--- v11/listLar11FunPro/les01-Init-V11-HelloWorld/routes/web.php
+++ v11/listLar11FunPro/les01-Init-V11-HelloWorld/routes/web.php
@@ -3,5 +3,24 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('first');
+    return view('welcome');
+});
+
+
+Route::get('/test', function() {
+    return view('welcome');
+});
+
+// Route::get("/home", function() {
+//     return view('home');
+// });
+
+Route::view('/home', 'home');
+Route::redirect("/home", '/');
+
+Route::view('/test-view-call', 'home');
+
+Route::get('/about-page/{name}', function($name) {
+    echo $name;
+    return view('about', ['name' => $name]);
 });
