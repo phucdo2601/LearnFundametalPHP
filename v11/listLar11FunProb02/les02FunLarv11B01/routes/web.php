35a3aa60253967a7a6de3e2bfaefa72f80d0da1a
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2024-10-02 11:29:58 +0700

    [les02-learn-fun-lar-v11-b01] 03. Integrate Html dashboard template b01

--- v11/listLar11FunProb02/les02FunLarv11B01/routes/web.php
+++ v11/listLar11FunProb02/les02FunLarv11B01/routes/web.php
@@ -4,9 +4,11 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('home.index');
+// });
+
+Route::get('/', [HomeController::class, 'home']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
