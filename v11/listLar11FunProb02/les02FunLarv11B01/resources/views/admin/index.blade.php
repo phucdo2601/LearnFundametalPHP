c3171cbca191144ebe1f9c0654bfc2c341889ede
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2024-10-02 13:14:36 +0700

    [les02-learn-fun-lar-v11-b01] 04. Integrate Html ADMIN dashboard template b01

--- v11/listLar11FunProb02/les02FunLarv11B01/resources/views/admin/index.blade.php
+++ v11/listLar11FunProb02/les02FunLarv11B01/resources/views/admin/index.blade.php
@@ -1,19 +1,33 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>This is admin dashboard page!</h1>
+<html>
+  @include('admin.css')
+  <body>
 
-    <!-- Authentication -->
-    <form method="POST" action="{{ route('logout') }}">
-        @csrf
+    @include('admin.header')
 
-        <input type="submit" value="Logout"/>
-    </form>
-</body>
+    <div class="d-flex align-items-stretch">
+      <!-- Sidebar Navigation-->
+      @include('admin.sidebar')
+      <!-- Sidebar Navigation end-->
+      <div class="page-content">
+        <div class="page-header">
+          <div class="container-fluid">
+            <h2 class="h5 no-margin-bottom">Dashboard</h2>
+          </div>
+        </div>
+        
+        @include('admin.body')
+
+      </div>
+    </div>
+    <!-- JavaScript files-->
+    <script src="{{asset('admincss/vendor/jquery/jquery.min.js')}}"></script>
+    <script src="{{asset('admincss/vendor/popper.js/umd/popper.min.js')}}"> </script>
+    <script src="{{asset('admincss/vendor/bootstrap/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('admincss/vendor/jquery.cookie/jquery.cookie.js')}}"> </script>
+    <script src="{{asset('admincss/vendor/chart.js/Chart.min.js')}}"></script>
+    <script src="{{asset('admincss/vendor/jquery-validation/jquery.validate.min.js')}}"></script>
+    <script src="{{asset('admincss/js/charts-home.js')}}"></script>
+    <script src="{{asset('admincss/js/front.js')}}"></script>
+  </body>
 </html>
