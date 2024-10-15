f990ead287c5c013b784dd7997a051aeee33b278
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2024-10-15 21:36:37 +0700

    [les02-learn-fun-lar-v11-b01] 08. Basic implement js confirmation for delete category b01

--- v11/listLar11FunProb02/les02FunLarv11B01/resources/views/admin/category.blade.php
+++ v11/listLar11FunProb02/les02FunLarv11B01/resources/views/admin/category.blade.php
@@ -73,7 +73,7 @@
               <tr>
                 <td>{{$data->category_name}}</td>
                 <td>
-                  <a class="btn btn-danger" href="{{ url('delete_category', $data->id) }}">Delete</a>
+                  <a class="btn btn-danger" onclick="confirmation(event)" href="{{ url('delete_category', $data->id) }}">Delete</a>
                 </td>
               </tr>
               @endforeach
@@ -88,6 +88,7 @@
     </div>
   </div>
   <!-- JavaScript files-->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js" integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
   <script src="{{asset('admincss/vendor/jquery/jquery.min.js')}}"></script>
   <script src="{{asset('admincss/vendor/popper.js/umd/popper.min.js')}}"> </script>
   <script src="{{asset('admincss/vendor/bootstrap/js/bootstrap.min.js')}}"></script>
@@ -96,6 +97,28 @@
   <script src="{{asset('admincss/vendor/jquery-validation/jquery.validate.min.js')}}"></script>
   <script src="{{asset('admincss/js/charts-home.js')}}"></script>
   <script src="{{asset('admincss/js/front.js')}}"></script>
+
+  <script type="text/javascript">
+  function confirmation(e) {
+    e.preventDefault();
+    
+    var urlToRedirect = e.currentTarget.getAttribute('href');
+    console.log(urlToRedirect);
+
+    swal({
+      title: "Are you sure to delete this?",
+      text: "You will not be able to recover this category!",
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+    })
+    .then((willCancel) => {
+      if (willCancel) {
+        window.location.href = urlToRedirect;
+      }
+    })
+  }
+  </script>
 </body>
 
 </html>
