83acd2962820769f5a2ebbc7ee7fa6d0dcfe571d
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2024-10-20 21:56:42 +0700

    [les02-learn-fun-lar-v11-b01] 16, Change redirect login page b01

--- v11/listLar11FunProb02/les02FunLarv11B01/resources/views/home/header.blade.php
+++ v11/listLar11FunProb02/les02FunLarv11B01/resources/views/home/header.blade.php
@@ -34,28 +34,39 @@
           </li>
         </ul>
         <div class="user_option">
-          <a href="{{url('/login')}}">
-            <i class="fa fa-user" aria-hidden="true"></i>
-            <span>
-              Login
-            </span>
-          </a>
+          @if (Route::has('login'))
+            @auth
+            <a href="">
+              <i class="fa fa-shopping-bag" aria-hidden="true"></i>
+            </a>
 
-          <a href="{{url('/register')}}">
-            <i class="fa fa-vcard" aria-hidden="true"></i>
-            <span>
-              Register
-            </span>
-          </a>
-
-          <a href="">
-            <i class="fa fa-shopping-bag" aria-hidden="true"></i>
-          </a>
-          <form class="form-inline ">
-            <button class="btn nav_search-btn" type="submit">
-              <i class="fa fa-search" aria-hidden="true"></i>
-            </button>
+            <form style="padding: 10px" method="POST" action="{{ route('logout') }}">
+              @csrf
+                <input class="btn btn-success" type="submit" value="Logout" />
           </form>
+          @else
+            <a href="{{url('/login')}}">
+              <i class="fa fa-user" aria-hidden="true"></i>
+              <span>
+                Login
+              </span>
+            </a>
+
+            <a href="{{url('/register')}}">
+              <i class="fa fa-vcard" aria-hidden="true"></i>
+              <span>
+                Register
+              </span>
+            </a>
+
+            @endauth
+
+          @endif
+
+          
+          
+
+          
         </div>
       </div>
     </nav>
