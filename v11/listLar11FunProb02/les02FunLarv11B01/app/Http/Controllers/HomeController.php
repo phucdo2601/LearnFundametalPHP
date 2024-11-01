91474cd6914a31777963549c37a09e94bb56cba3
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2024-11-01 13:53:18 +0700

    [les02-learn-fun-lar-v11-b01] 17. Display list pro to home page b01

--- v11/listLar11FunProb02/les02FunLarv11B01/app/Http/Controllers/HomeController.php
+++ v11/listLar11FunProb02/les02FunLarv11B01/app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -12,6 +13,13 @@ class HomeController extends Controller
     }
 
     public function home() {
-        return view('home.index');
+
+        $list_products = Product::all();
+        return view('home.index', compact('list_products'));
+    }
+
+    public  function login_home() {
+        $list_products = Product::all();
+        return view('home.index', compact('list_products'));
     }
 }
