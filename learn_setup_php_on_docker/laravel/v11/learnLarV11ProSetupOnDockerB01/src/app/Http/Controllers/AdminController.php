b4b9c8337dedac21a0982631dcb373a77743056e
phucdo2601 <ngocphucdo2601@gmail.com>
2024-11-04 22:13:35 +0700

    [setup-lar-11-pro-on-docker-b01] 07. Basic set up update brand of pro b01

--- learn_setup_php_on_docker/laravel/v11/learnLarV11ProSetupOnDockerB01/src/app/Http/Controllers/AdminController.php
+++ learn_setup_php_on_docker/laravel/v11/learnLarV11ProSetupOnDockerB01/src/app/Http/Controllers/AdminController.php
@@ -6,56 +6,99 @@ use App\Models\Brand;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\File;
 use  Intervention\Image\Laravel\Facades\Image;
 
 class AdminController extends Controller
 {
     //
-    public function index() {
+    public function index()
+    {
         return view("admin.index");
     }
 
-    public function brands() {
-        $brandList = Brand::orderBy("id","desc")->paginate(10);
+    public function brands()
+    {
+        $brandList = Brand::orderBy("id", "desc")->paginate(10);
         return view("admin.brands", compact("brandList"));
     }
 
     // Routing to admin add-brand page
-    public function add_brand() {
+    public function add_brand()
+    {
         return view("admin.brand-add");
     }
 
     // SAVE Brands
-    public function brand_store(Request $request) {
+    public function brand_store(Request $request)
+    {
         $request->validate([
             'name' => 'required',
-          'slug' => 'required|unique:brands,slug',
+            'slug' => 'required|unique:brands,slug',
+            'image' => 'mimes:png,jpg,jpeg|max:2048'
         ]);
 
         $brand = new Brand();
         $brand->name = $request->name;
-        $brand->slug = Str::slug($request->slug);
+        $brand->slug = Str::slug($request->name);
+
+
         $image = $request->file('image');
         $file_extension = $request->file('image')->extension();
-        $file_name = Carbon::now()->timestamp . ".".$file_extension;
+        $file_name = Carbon::now()->timestamp . "." . $file_extension;
         $this->GenerateBrandThumbnailsImage($image, $file_name);
         $brand->image = $file_name;
 
         $brand->save();
 
-        return redirect()->route('admin.brands')->with('status','
+        return redirect()->route('admin.brands')->with('status', '
             Brand has been added successfully!
         ');
-
     }
 
-    public function GenerateBrandThumbnailsImage($image, $imageName) {
+    public function brand_edit($id)
+    {
+        $brand = Brand::find($id);
+        return view('admin.brand-edit', compact('brand'));
+    }
+
+    public function brand_update(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'slug' => 'required|unique:brands,slug,' . $request->id,
+            'image' => 'mimes:png,jpg,jpeg|max:2048'
+        ]);
+
+        $brand = Brand::find($request->id);
+        $brand->name = $request->name;
+        $brand->slug = $request->slug;
+
+        if ($request->hasFile('image')) {
+            if (File::exists(public_path("uploads/brands") . '/' . $brand->image)) {
+                File::delete(public_path("uploads/brands") . '/' . $brand->image);
+            }
+            $image = $request->file('image');
+            $file_extension = $request->file('image')->extension();
+            $file_name = Carbon::now()->timestamp . "." . $file_extension;
+            $this->GenerateBrandThumbnailsImage($image, $file_name);
+            $brand->image = $file_name;
+        }
+        $brand->save();
+
+        return redirect()->route('admin.brands')->with('status', '
+            Brand has been update successfully!
+        ');
+    }
+
+    public function GenerateBrandThumbnailsImage($image, $imageName)
+    {
         $destinationPath = public_path("uploads/brands");
         $img = Image::read($image->path());
 
         $img->cover(124, 124, 'top');
-        $img->resize(124, 124, function($constraint) {
+        $img->resize(124, 124, function ($constraint) {
             $constraint->aspectRatio();
-        })->save($destinationPath.'/'.$imageName);
+        })->save($destinationPath . '/' . $imageName);
     }
 }
