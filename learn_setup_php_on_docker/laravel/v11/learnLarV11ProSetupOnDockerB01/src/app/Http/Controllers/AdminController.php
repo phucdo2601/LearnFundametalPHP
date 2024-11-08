c112bcc09bd542302a3478e1c92414f2eb784ee6
phucdo2601 <ngocphucdo2601@gmail.com>
2024-11-08 22:21:19 +0700

    [setup-lar-11-pro-on-docker-b01] 13. Basic impl admin add product b01

--- learn_setup_php_on_docker/laravel/v11/learnLarV11ProSetupOnDockerB01/src/app/Http/Controllers/AdminController.php
+++ learn_setup_php_on_docker/laravel/v11/learnLarV11ProSetupOnDockerB01/src/app/Http/Controllers/AdminController.php
@@ -210,4 +210,95 @@ class AdminController extends Controller
         $list_products = Product::orderBy('created_at', 'DESC')->paginate(10);
         return view('admin.products', compact('list_products'));
     }
+
+    public function products_add()
+    {
+        $brandList = Brand::select('id', 'name')->orderBy('name')->get();
+        $list_categories = Category::select('id', 'name')->orderBy('name')->get();
+
+        return view('admin.product_add', compact('list_categories', 'brandList'));
+    }
+
+    public function product_store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'slug' => 'required|unique:products,slug',
+            'category_id' => 'required',
+            'brand_id' => 'required',
+            'short_description' => 'required',
+            'description' => 'required',
+            'regular_price' => 'required',
+            'sale_price' => 'required',
+            'SKU' => 'required',
+            'stock_status' => 'required',
+            'featured' => 'required',
+            'quantity' => 'required',
+            'image' => 'required|mimes:png,jpg,jpeg|max:2048'
+        ]);
+
+        $product = new Product();
+        $product->name = $request->name;
+        $product->slug = Str::slug($request->name);
+        $product->short_description = $request->short_description;
+        $product->description = $request->description;
+        $product->regular_price = $request->regular_price;
+        $product->sale_price = $request->sale_price;
+        $product->SKU = $request->SKU;
+        $product->stock_status = $request->stock_status;
+        $product->featured = $request->featured;
+        $product->quantity = $request->quantity;
+        $product->category_id = $request->category_id;
+        $product->brand_id = $request->brand_id;
+
+        $current_timestamp = Carbon::now()->timestamp;
+
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $imageName = $current_timestamp . '.' . $image->extension();
+            $this->GenerateProductThumbnailImage($image, $imageName);
+            $product->image = $imageName;
+        }
+
+        $gallery_arr = array();
+        $gallery_images = "";
+        $counter = 1;
+
+        if ($request->hasFile('images')) {
+            $allowedFileExtensions = ['jpg', 'png', 'jpeg'];
+            $files = $request->file('images');
+            foreach ($files as $file) {
+                $gextension = $file->getClientOriginalExtension();
+                $gcheck = in_array($gextension, $allowedFileExtensions);
+                if ($gcheck) {
+                    $gfilename = $current_timestamp . '-' . $counter . '.' . $gextension;
+                    $this->GenerateProductThumbnailImage($file, $gfilename);
+                    array_push($gallery_arr, $gfilename);
+                    $counter = $counter + 1;
+                }
+            }
+            $gallery_images = implode(',', $gallery_arr);
+        }
+        $product->images = $gallery_images;
+        $product->save();
+
+        return redirect()->route('admin.products')->with('status', 'Product has been added successfully');
+    }
+
+    public function GenerateProductThumbnailImage($image, $imageName)
+    {
+        $destinationPathThumbnail = public_path('uploads/products/thumbnalis');
+        $destinationPath = public_path('uploads/products');
+        $img = Image::read($image->path());
+
+        $img->cover(540, 689, 'top');
+
+        $img->resize(540,  689, function ($constraint) {
+            $constraint->aspectRatio();
+        })->save($destinationPath . '/' . $imageName);
+
+        $img->resize(540, 689, function ($constraint) {
+            $constraint->aspectRatio();
+        })->save($destinationPathThumbnail . '/' . $imageName);
+    }
 }
--- learn_setup_php_on_docker/laravel/v11/learnLarV11ProSetupOnDockerB01/src/resources/views/admin/products.blade.php
+++ learn_setup_php_on_docker/laravel/v11/learnLarV11ProSetupOnDockerB01/src/resources/views/admin/products.blade.php
@@ -32,10 +32,13 @@
                         </div>
                     </form>
                 </div>
-                <a class="tf-button style-1 w208" href="add-product.html"><i
+                <a class="tf-button style-1 w208" href="{{ route('admin.product.add', []) }}"><i
                         class="icon-plus"></i>Add new</a>
             </div>
             <div class="table-responsive">
+                @if (Session::has('status'))
+                    <p class="alert alert-success">{{Session::get('status')}}</p>
+                @endif
                 <table class="table table-striped table-bordered">
                     <thead>
                         <tr>
@@ -102,7 +105,7 @@
 
             <div class="divider"></div>
             <div class="flex items-center justify-between flex-wrap gap10 wgp-pagination">
-                {{$list_products->links('pagination:bootstrap-5')}}
+                {{$list_products->links('pagination::bootstrap-5')}}
 
             </div>
         </div>
