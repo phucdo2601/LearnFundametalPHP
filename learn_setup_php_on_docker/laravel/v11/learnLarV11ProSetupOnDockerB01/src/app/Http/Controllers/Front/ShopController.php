53e9fb5abf1b1e4b31538fe17a462dac2a67c1d4
phucdo2601 <ngocphucdo2601@gmail.com>
2025-03-03 21:39:52 +0700

    [setup-lar-11-pro-on-docker-b01] 23. Basic impl Product Sorting and Products Per Page

--- learn_setup_php_on_docker/laravel/v11/learnLarV11ProSetupOnDockerB01/src/app/Http/Controllers/Front/ShopController.php
+++ learn_setup_php_on_docker/laravel/v11/learnLarV11ProSetupOnDockerB01/src/app/Http/Controllers/Front/ShopController.php
@@ -9,10 +9,42 @@ use Illuminate\Http\Request;
 class ShopController extends Controller
 {
 
-    public function index()
+    public function index(Request $request)
     {
-        $listProducts = Product::orderBy('created_at', 'DESC')->paginate(12);
-        return view('front.shop', compact('listProducts'));
+        $size = $request->query('size') ? $request->query('size') : 12;
+        $o_Column = "";
+        $o_order = "";
+        $order = $request->query('order') ? $request->query('order') : -1;
+
+        switch ($order) {
+            case 1:
+                $o_Column = 'created_at';
+                $o_order = 'DESC';
+                break;
+
+            case 2:
+                $o_Column = 'created_at';
+                $o_order = 'ASC';
+                break;
+
+            case 3:
+                $o_Column = 'regular_price';
+                $o_order = 'ASC';
+                break;
+
+            case 4:
+                $o_Column = 'regular_price';
+                $o_order = 'DESC';
+                break;
+
+            default:
+                $o_Column = 'id';
+                $o_order = 'DESC';
+                break;
+        }
+
+        $listProducts = Product::orderBy($o_Column, $o_order)->paginate($size);
+        return view('front.shop', compact('listProducts', 'size', 'order'));
     }
 
     public function product_details($product_slug)
