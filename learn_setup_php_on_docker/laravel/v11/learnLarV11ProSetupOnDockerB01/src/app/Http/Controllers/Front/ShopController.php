84312989a83f80f860e917fb854d14b161444519
phucdo2601 <ngocphucdo2601@gmail.com>
2025-03-05 21:56:17 +0700

    [setup-lar-11-pro-on-docker-b01] 26. Products filter by price

--- learn_setup_php_on_docker/laravel/v11/learnLarV11ProSetupOnDockerB01/src/app/Http/Controllers/Front/ShopController.php
+++ learn_setup_php_on_docker/laravel/v11/learnLarV11ProSetupOnDockerB01/src/app/Http/Controllers/Front/ShopController.php
@@ -19,6 +19,9 @@ class ShopController extends Controller
         $order = $request->query('order') ? $request->query('order') : -1;
         $f_brands = $request->query('brands');
         $f_categories = $request->query('categories');
+        // filter by price
+        $min_price = $request->query('min') ? $request->query('min') : 1;
+        $max_price = $request->query('max') ? $request->query('max') : 1000;
 
         switch ($order) {
             case 1:
@@ -55,8 +58,12 @@ class ShopController extends Controller
             ->where(function ($query) use ($f_categories) {
                 $query->whereIn('category_id', explode(',', $f_categories))->orWhereRaw("'" . $f_categories . "'=''");
             })
+            ->where(function ($query) use ($min_price, $max_price) {
+                $query->whereBetween('regular_price', [$min_price, $max_price])
+                    ->orWhereBetween('sale_price', [$min_price, $max_price]);
+            })
             ->orderBy($o_Column, $o_order)->paginate($size);
-        return view('front.shop', compact('listProducts', 'size', 'order', 'brands', 'f_brands', 'categories', 'f_categories'));
+        return view('front.shop', compact('listProducts', 'size', 'order', 'brands', 'f_brands', 'categories', 'f_categories', 'min_price', 'max_price'));
     }
 
     public function product_details($product_slug)
